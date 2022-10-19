2b61c7e78dd5c6f4e6e9a1ed30c7a509d2c11b6e
Merit Gloria Ogudu <gmerit2030@gmail.com>
2022-10-19 11:58:35 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - to print alphabet in lowercase alphabet to stdout.
+ * Return: 0 when successful.
+ */
+void print_alphabet(void)
+{
+	char o = 'a';
+
+	while (o <= 'z')
+	{
+		_putchar(o);
+		o++;
+	}
+	_putchar('\n');
+}
