eaf1f4413940816fd3724eccbb6c7226c2874e97
Gloria Merit Ogudu <gmerit2030@gmail.com>
2023-05-10 20:40:50 -0700

    linear

--- 0x1E-search_algorithms/0-main.c
+++ 0x1E-search_algorithms/0-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "search_algos.h"
+#include "0-linear.c/search_algos.h"
 
 /**
  * main - Entry point
@@ -17,4 +17,5 @@ int main(void)
     printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
     printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
     printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+    return (EXIT_SUCCESS);
 }
