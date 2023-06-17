f6edd6d95b12f7a6e4897995da2f5804df6f28e3
Gloria Merit Ogudu <gmerit2030@gmail.com>
2023-06-17 16:52:05 -0700

    library

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * check the code
+ *
+ * return: Always EXIT_SUCCESS.
+ */
+
+int main(void)
+{
+	printf("%d\n", _strlen("My Dyn Lib"));
+	return (EXIT_SUCCESS);
+}
